78a469ed0e5934e0153d696be8d9d507cc28cde4
Aodai <Aodai@users.noreply.github.com>
2021-07-02 19:01:39 +0300

    Initial commit

---
+++ ProfessorLookup/Models/Professor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProfessorLookup.Models
+{
+    public record Professor
+    {
+        public string FirstName { get; init; }
+        public string LastName { get; init; }
+        public string Email { get; init; }
+    }
+}
